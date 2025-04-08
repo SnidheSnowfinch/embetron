bfb5cb456987dcdf44c5731d62da6301daf8e754
snidhedinesh <snidhe2016@gmail.com>
2025-04-08 18:41:30 +0530

    first commit

---
+++ includes/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rooms page</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="https://kit.fontawesome.com/YOUR-KIT-ID.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Anek+Latin:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="bootstrap/style.css">
+
+</head>
+
+<body>
+
+    <div class="top-bar">
+        <div>
+            Mon - Fri 8:00 - 7:00 / Sunday 8:00 - 5:00 | Flat no: 2/A, Shreevaysh terrace, Warje, Pune - 411052
+            <label for="">
+                <a href="#">info@omkretansolar.com</a> |
+                <a href="#">Follow Us On</a>
+            </label>
+        </div>
+        <div>
+
+            <a href="#"><i class="bi bi-facebook"></i></a>
+            <a href="#"><i class="bi bi-twitter"></i></a>
+            <a href="#"><i class="bi bi-linkdin"></i></a>
+            <a href="#"><i class="bi bi-instagram"></i></a>
+            <a href="#"><i class="bi bi-youtube"></i></a>
+
+
+
+        </div>
+    </div>
+
+    <nav class="navbar">
+        <div class="top-div"><img class="top-img" src="bootstrap/images/nav-image.png" alt="">
+            <div class="logo top-img-inner"><img src="bootstrap/images/logo-img-top.png" alt=""></div>
+        </div>
+        <ul>
+
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Pages</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Project</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div class="link-div"><a href=""><img src="bootstrap/images/search-green.png" alt=""></a>
+            <a href=""><img src="bootstrap/images/grid.png" alt=""></a>
+            <a href="#" class="top-btn">Call For Help: +91 0000000000</a>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,141 @@
+<?php include 'includes/header.php' ?>
+    <section class="hero">
+        <div class="hero-content">
+            <h2>ENSURE A GREEN FUTURE</h2>
+            <h1>Affordable <br /> Power Solutions</h1>
+            <a href="#" class="top-btn">Get A Quote ➤</a>
+        </div>
+    </section>
+    <section class="section-img">
+        <div class="left">
+            <img src="bootstrap/images/solar.png" alt="Solar Panels" />
+            <div class="badge">
+                <img src="bootstrap/images/OverlayBlur.png" alt="">
+            </div>
+        </div>
+
+        <div class="right">
+            <h5><i class="bi bi-sun"></i>Embetron – Solar & Renewable Energy<i class="bi bi-sun"></i></h5>
+            <h2>We Strive To Lead The Way In The Solar Energy Industry.</h2>
+            <p>
+                Solar panels with PV cells convert sunlight directly into electricity. PV panels are often installed on
+                rooftops, in solar farms, and even in smaller portable chargers.
+            </p>
+
+            <div class="features">
+                <span><img src="bootstrap/images/green-tick.png" alt=""> Clean and Renewable</span>
+                <span><img src="bootstrap/images/green-tick.png" alt=""> Low Maintenance</span>
+                <span><img src="bootstrap/images/green-tick.png" alt=""> Energy Independence</span>
+                <span><img src="bootstrap/images/green-tick.png" alt=""> Solar Scalability</span>
+            </div>
+
+            <a href="#" class="top-btn">More About Us ➤</a>
+        </div>
+    </section>
+
+    <section class="service-boxes">
+        <div class="service">
+
+            <img src="bootstrap/images/energy.png" alt="">
+            <h4>Energy Solutions</h4>
+            <p>Solar panels with PV cells convert sunlight directly into electricity, commonly used on rooftops or solar
+                farms.</p>
+        </div>
+
+        <div class="service">
+            <img src="bootstrap/images/global.png" alt="">
+            <h4>Global Expertise</h4>
+            <p>In-depth understanding and proficiency in delivering solar projects around the globe.</p>
+        </div>
+
+        <div class="service">
+            <img src="bootstrap/images/home.png" alt="">
+            <h4>Home Appliance</h4>
+            <p>Efficient energy use for electrical or mechanical devices to ease daily life.</p>
+        </div>
+
+        <div class="service">
+            <img src="bootstrap/images/installation.png" alt="">
+            <h4>Easy Installation</h4>
+            <p>Quick and minimal-effort process to install and operate solar energy systems.</p>
+        </div>
+    </section>
+    <section class="fecility-section">
+        <div class="fecility-inner">
+            <div class="fecility-box">
+                <img src="bootstrap/images/fec-icon-1.png" alt="">
+                <h5>10,000,000+</h5>
+                <p>Instances of fault activity captured
+                    on LV networks</p>
+            </div>
+            <div class="fecility-box-center">
+                <img src="bootstrap/images/fec-icon-2.png" alt="">
+                <h5>10,000,000+</h5>
+                <p>Instances of fault activity captured
+                    on LV networks</p>
+            </div>
+            <div class="fecility-box">
+                <img src="bootstrap/images/fec-icon-3.png" alt="">
+                <h5>10,000,000+</h5>
+                <p>Instances of fault activity captured
+                    on LV networks</p>
+            </div>
+        </div>
+    </section>
+    <section class="section-img">
+        <div class="left">
+            <img src="bootstrap/images/solar.png" alt="Solar Panels" />
+
+        </div>
+
+        <div class="right">
+            <h5><i class="bi bi-sun"></i>Embetron – Solar & Renewable Energy<i class="bi bi-sun"></i></h5>
+            <h2>We Strive To Lead The Way In The Solar Energy Industry.</h2>
+            <p>
+                Solar panels with PV cells convert sunlight directly into electricity. PV panels are often installed on
+                rooftops, in solar farms, and even in smaller portable chargers.
+            </p>
+            <div class="border-div">
+                <div class="support-div"><img src="bootstrap/images/proffesional.png" alt="">
+                    <div class="mx-3">
+                        <h5>Professional Solar Expert</h5>
+                        <p>Solar panels, also referred to as solar modules, are made up of multiple</p>
+                    </div>
+                </div>
+                <div class="support-div"><img src="bootstrap/images/personal.png" alt="">
+                    <div class="mx-3">
+                        <h5>24/7 Customer Support</h5>
+                        <p>solar cells arranged in a grid. They are installed on rooftops, solar
+                            farms, and other suitable..</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </section>
+    <div class="gray-div">
+    <section class="work-section">
+        
+        <h5><i class="bi bi-sun"></i>Embetron – Solar & Renewable Energy<i class="bi bi-sun"></i></h5>
+        <div class="work-div">
+            <h2>How We Work Process In
+                Solar Energy</h2>
+        </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-12"></div>
+                <div class="col-lg-3 col-md-6 col-sm-12"></div>
+                <div class="col-lg-3 col-md-6 col-sm-12"></div>
+                <div class="col-lg-3 col-md-6 col-sm-12"></div>
+
+
+
+            </div>
+
+
+    </section>
+</div>
+</body>
+
+</html>
